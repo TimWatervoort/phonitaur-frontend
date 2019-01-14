ccebc23e68c05a5156ece437bd854580372e9655
Tim Watervoort <twwatervoort@gmail.com>
2019-01-14 15:37:05 -0700

    minor style changes

--- src/components/LanguageCard/LanguageCard.js
+++ src/components/LanguageCard/LanguageCard.js
@@ -9,7 +9,7 @@ class LanguageCard extends Component {
         <div className={`${alphabet.textColor} card lang-card mt-3 text-center`}>
         <img className="card-img lang-card-img" src={`${alphabet.img}`} alt="Alphabet"/>
           <div className="d-flex flex-column card-img-overlay">
-            <h3 className='card-title lang-text'>{alphabet.name}</h3>
+            <h3 className='card-title lang-text'><strong>{alphabet.name}</strong></h3>
 
             <button className='mt-auto mx-auto cont-button btn btn-primary lang-text'>Continue!</button>
           </div>
