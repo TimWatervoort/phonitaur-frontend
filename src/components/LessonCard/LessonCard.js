18d458785719f1885a5a9795b8ff9217e3b82e09
Tim Watervoort <twwatervoort@gmail.com>
2019-01-16 14:12:38 -0700

    lesson cards are clickable

--- src/components/LessonCard/LessonCard.js
+++ src/components/LessonCard/LessonCard.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import './LessonCard.css';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getLessons } from '../../actions/index';
 
 class LessonCard extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      clicked: false
+    }
+    this.redirectToLesson = this.redirectToLesson.bind(this);
+  }
+
+  redirectToLesson() {
+    this.setState({clicked: true});
+  }
+
   render(){
 
     const { lesson, alph } = this.props
 
     return(
       <div className='col-lg-4 col-md-12'>
-        <div className='card' id='lesson-card'>
+        {this.state.clicked ? <Redirect to={`/lesson/${lesson.id}`}/> : null}
+        <div onClick={this.redirectToLesson} className='card' id='lesson-card'>
           <div className='card-body lesson-card-body'>
             <h4 className='card-title text-white user-text'>{lesson.name}</h4>
             <h5 className='card-subtitle text-grey user-text'>{alph} Lesson {lesson.level}</h5>
