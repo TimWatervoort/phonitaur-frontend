c438a85ae38e597b7fa3eead58f2ac8c7dfac731
Tim Watervoort <twwatervoort@gmail.com>
2019-01-29 10:53:09 -0700

    style changes

--- src/components/ProfileCard/EditableCard.js
+++ src/components/ProfileCard/EditableCard.js
@@ -14,14 +14,14 @@ class EditableCard extends Component {
 
             <div className='row'>
               <div className='col-4'>
-                <img className="user-img" src={user.img ? user.img : minotaur} alt="User"/>
+                <img className="user-img img-fluid" src={user.img ? user.img : minotaur} alt="User"/>
               </div>
               <div className='col-8 text-left'>
                 <form onSubmit={onSubmit}>
-                  <input className='form-control mb-1' type='text' name='username' onChange={handleChange} placeholder={`Username: ${user.username}`}/>
-                  <input className='form-control mb-1' type='text' name='mother_alphabet' onChange={handleChange} placeholder={`Native Alphabet: ${user.mother_alphabet}`}/>
-                  <input className='form-control' type='url' name='img' onChange={handleChange} placeholder={`Image URL: ${user.img}`}/>
-                  <button className='btn btn-danger user-text mx-auto mt-3' type='submit'>Submit</button>
+                  <input className='form-control user-text mb-1' type='text' name='username' onChange={handleChange} placeholder={`Username: ${user.username}`}/>
+                  <input className='form-control user-text mb-1' type='text' name='mother_alphabet' onChange={handleChange} placeholder={`Native Alphabet: ${user.mother_alphabet}`}/>
+                  <input className='form-control user-text' type='url' name='img' onChange={handleChange} placeholder={`Image URL: ${user.img}`}/>
+                  <button className='btn btn-danger btn-text mx-auto mt-3' type='submit'>Submit</button>
                 </form>
               </div>
             </div>
