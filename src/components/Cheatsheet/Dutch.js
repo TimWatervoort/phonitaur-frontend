6e65e438731f649c851bc189c7e9de64a0f4ebb5
Tim Watervoort <twwatervoort@gmail.com>
2019-02-01 10:14:34 -0700

    changed ipa to symbols

--- src/components/Cheatsheet/Dutch.js
+++ src/components/Cheatsheet/Dutch.js
@@ -1,3 +1,16 @@
-const dutch = [{char: 'ch', english: 'kh',ipa:'kh'}, {char: 'g', english: 'kh',ipa:'kh'}, {char: 'gh', english: 'kh',ipa:'kh'}, {char: 'sch', english: 'skh',ipa:'skh'}, {char: 'j', english: 'y',ipa:'je'}, {char: 'w', english: 'v',ipa:'v'}, {char: 'ee', english: 'ay',ipa:'ei'}, {char: 'ei', english: 'ay',ipa:'ee'}, {char: 'ij', english: 'ay',ipa:'ei'}, {char: 'u', english: 'rounded i',ipa:'y'}, {char: 'y', english: 'ay',ipa:'ei'}, {char: 'ui', english: 'rounded uy',ipa:'ui'}, {char: 'oe', english: 'oo',ipa:'oo'}];
+const dutch = [
+  {char: 'ch', english: 'kh',ipa:'x'},
+  {char: 'g', english: 'kh',ipa:'x'},
+  {char: 'gh', english: 'kh',ipa:'x'},
+  {char: 'sch', english: 'skh',ipa:'sx'},
+  {char: 'j', english: 'y',ipa:'j'},
+  {char: 'w', english: 'v',ipa:'v'},
+  {char: 'ee', english: 'ay',ipa:'ei'},
+  {char: 'ei', english: 'ay',ipa:'ee'},
+  {char: 'ij', english: 'ay',ipa:'ei'},
+  {char: 'u', english: 'rounded i',ipa:'y'},
+  {char: 'y', english: 'ay',ipa:'ei'},
+  {char: 'ui', english: 'rounded uy',ipa:'ui'},
+  {char: 'oe', english: 'oo',ipa:'u'}];
 
 export default dutch;
