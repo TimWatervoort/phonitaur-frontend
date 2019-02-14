b27dbefa171f9a4095df100eae6c6b3f76436a26
Tim Watervoort <twwatervoort@gmail.com>
2019-02-14 14:45:11 -0700

    can edit languages

--- src/components/Admin/AdminEdit.js
+++ src/components/Admin/AdminEdit.js
@@ -1,19 +1,87 @@
 import React, { Component } from 'react';
 import Navbar from '../Navbar/Navbar';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { getLanguages } from '../../actions/index';
 
 class AdminEdit extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      languageId: '',
+      languageName: '',
+      languageImg: '',
+      languageColor: '',
+      lessonName: '',
+      lessonLanguage: '',
+      lessonText: '',
+      lessonLevel: '',
+      lessonIcon: '',
+      questionText: '',
+      questionAnswer: '',
+      questionName: '',
+      questionLesson: ''
+    }
+
+  }
+
+  langEditInit = async e => {
+    const response = await fetch(`https://phonitaur-backend.herokuapp.com/language/${e.target.id}`);
+    const json = await response.json();
+    this.setState({
+      languageId: json.id,
+      languageName: json.name,
+      languageImg: json.img,
+      languageColor: json.text_color
+    });
+  }
+
+  langEditSubmit = async e => {
+    e.preventDefault();
+    const data = {
+      name: this.state.languageName,
+      img: this.state.languageImg,
+      text_color: this.state.languageColor
+    }
+    const response = await fetch(`https://phonitaur-backend.herokuapp.com/language/${this.state.languageId}`, {
+      method: 'PATCH',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(data)
+    });
+    const json = await response.json();
+    console.log(json);
+  }
+
+  handleLanguage = e => {
+    const key = `language${e.target.name}`;
+    this.setState({ [key]: e.target.value });
+  }
+
   render() {
+
+    const { languages } = this.props;
+
+    // const theBois = [{name: 'Gerald'},{name: 'Egbert'},{name:'Azvyrakhys'}];
+
     return(
       <div>
         <Navbar leftButton={'Home'} leftLink={`/home`} rightButton={'Catalog'} rightLink={`/catalog`} />
         <h4 className='text-white mx-auto text-center my-4'>Edit Stuff</h4>
         <div className='container'>
+
           <div className='row'>
             <div className='col'>
-            <form onSubmit={this.addLanguage} className='mb-4'>
-              <input type='text' name='Name' onChange={this.handleLanguage} value={this.languageName} placeholder='Language name' autoComplete='off' className='my-2 form-control'/>
-              <input type='text' name='Img' onChange={this.handleLanguage} value={this.languageImg} placeholder='Language image' autoComplete='off' className='my-2 form-control'/>
-              <input type='text' name='Color' onChange={this.handleLanguage} value={this.languageColor} placeholder='Language text color' autoComplete='off' className='my-2 form-control'/>
+              {Array.isArray(languages) ? languages.map((x,i) => <span key={i} id={x.id} onClick={this.langEditInit} className='mx-1 badge badge-dark'>{x.name}</span>) : null}
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col'>
+            <form onSubmit={this.langEditSubmit} className='mb-4'>
+              <input type='text' name='Name' onChange={this.handleLanguage} value={this.state.languageName} placeholder='Language name' autoComplete='off' className='my-2 form-control'/>
+              <input type='text' name='Img' onChange={this.handleLanguage} value={this.state.languageImg} placeholder='Language image' autoComplete='off' className='my-2 form-control'/>
+              <input type='text' name='Color' onChange={this.handleLanguage} value={this.state.languageColor} placeholder='Language text color' autoComplete='off' className='my-2 form-control'/>
               <button className='btn btn-danger mx-auto text-center' type='submit'>Submit Language</button>
             </form>
 
@@ -42,4 +110,12 @@ class AdminEdit extends Component {
   }
 }
 
-export default AdminEdit;
+const mapStateToProps = state => ({
+  languages: state.languages
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+  getLanguages
+}, dispatch);
+
+export default connect(mapStateToProps,mapDispatchToProps)(AdminEdit);
